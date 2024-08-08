3d0e37cb2aaf8c7ec8eb59c42d4f52045dda878d
JBevc <141636441+JBevc@users.noreply.github.com>
2024-08-07 21:06:58 -0300

    Parte B

--- Unidad 1/UT1-TA2/ut1-ta2/src/main/java/ut1ta2/UtilMath.java
+++ Unidad 1/UT1-TA2/ut1-ta2/src/main/java/ut1ta2/UtilMath.java
@@ -4,14 +4,46 @@ package ut1ta2;
  * Hello world!
  */
 public final class UtilMath {
-    private UtilMath() {
+
+    public static void main(String[] args) {
+
+        int numero = 7;
+        int resultado = suma(numero);
+        System.out.println(resultado);
+        System.out.println("Hello World!");
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static boolean isPrime(long n) {
+        boolean prime = true;
+        for (long i = 3; i <= Math.sqrt(n); i += 2)
+            if (n % i == 0) {
+                prime = false;
+                break;
+        }
+        if (( n%2 !=0 && prime && n > 2) || n == 2) {
+            return true;
+        } 
+        else {
+            return false;
+        }
+    }
+
+    public static int suma(int num) {
+        int i = 0;
+        int suma = 0;
+
+        if (isPrime(num) == true) {
+            while (i<num) {
+                suma += i;
+                i += 2;
+            }
+        } else {
+            i=1;
+            while (i<=num) {
+                suma += i;
+                i += 2;
+            }
+        }
+        return suma;
     }
 }
