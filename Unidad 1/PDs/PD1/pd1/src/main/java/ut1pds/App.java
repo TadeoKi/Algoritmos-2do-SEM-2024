9c57cb256a070534c7355bc764a942672fd01ac8
Tadeo <tadeo.ki@correo.ucu.edu.uy>
2024-08-09 19:06:23 -0300

    adicion pd1

---
+++ Unidad 1/PDs/PD1/pd1/src/main/java/ut1pds/App.java
@@ -0,0 +1,21 @@
+package ut1pds;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        double numero1=1, numero2=2, numero3=3;
+        
+        Multsuma multisuma = new Multsuma();
+        double result = multisuma.multsuma(numero1,numero2,numero3);
+        System.out.println(result);
+    }
+}
---
+++ Unidad 1/PDs/PD1/pd1/src/main/java/ut1pds/Multsuma.java
@@ -0,0 +1,10 @@
+package ut1pds;
+
+public class Multsuma {
+    public double multsuma(double num1, double num2, double num3){
+        double resultado = 0;
+        resultado = num1*num2 +num3;
+        
+        return resultado;
+    }
+}
